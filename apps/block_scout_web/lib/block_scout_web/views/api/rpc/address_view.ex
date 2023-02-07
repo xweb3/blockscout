322cea8f5e0a7ba5caf37e444cc90cacd8f9103f
Jaycelv <supercamel001@gmail.com>
2023-02-07 20:59:31 +0800

    fix: there is history data in transaction that l1 gas price is nil

--- apps/block_scout_web/lib/block_scout_web/views/api/rpc/address_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/rpc/address_view.ex
@@ -119,10 +119,10 @@ defmodule BlockScoutWeb.API.RPC.AddressView do
       "input" => "#{transaction.input}",
       "contractAddress" => "#{transaction.created_contract_address_hash}",
       "cumulativeGasUsed" => "#{transaction.cumulative_gas_used}",
-      "l1GasPrice" => "#{transaction.l1_gas_price}",
-      "l1GasUsed" => "#{transaction.l1_gas_used}",
-      "l1Fee" => "#{transaction.l1_fee}",
-      "l1FeeScalar" => "#{transaction.l1_fee_scalar}",
+      "l1GasPrice" => if(transaction.l1_gas_price == nil, do: "0", else: transaction.l1_gas_price),
+      "l1GasUsed" => if(transaction.l1_gas_used == nil, do: "0", else: transaction.l1_gas_used),
+      "l1Fee" => if(transaction.l1_fee == nil, do: "0", else: transaction.l1_fee),
+      "l1FeeScalar" =>if(transaction.l1_fee_scalar == nil, do: "0", else: transaction.l1_fee_scalar),
       "gasUsed" => "#{transaction.gas_used}",
       "confirmations" => "#{transaction.confirmations}"
     }
