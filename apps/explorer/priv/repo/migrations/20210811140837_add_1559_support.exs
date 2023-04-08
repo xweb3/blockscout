b2a6437a69e5e66540bebf88917e0d8c82b481c1
Jaycelv <supercamel001@gmail.com>
2023-04-08 11:06:31 +0800

    #175 feat: upgrade 2 blockscoutv5.0.0-beta

--- apps/explorer/priv/repo/migrations/20210811140837_add_1559_support.exs
+++ apps/explorer/priv/repo/migrations/20210811140837_add_1559_support.exs
@@ -1,15 +1,16 @@
-defmodule Explorer.Repo.Migrations.Add1559Support do
-  use Ecto.Migration
+# TODO(Jyace) for QA, these field has been added, but not added in testnet
+#defmodule Explorer.Repo.Migrations.Add1559Support do
+#  use Ecto.Migration
 
-  def change do
-    alter table(:transactions) do
-      add(:max_priority_fee_per_gas, :numeric, precision: 100, null: true)
-      add(:max_fee_per_gas, :numeric, precision: 100, null: true)
-      add(:type, :integer, null: true)
-    end
+#  def change do
+#    alter table(:transactions) do
+#      add(:max_priority_fee_per_gas, :numeric, precision: 100, null: true)
+#      add(:max_fee_per_gas, :numeric, precision: 100, null: true)
+#      add(:type, :integer, null: true)
+#    end
 
-    alter table(:blocks) do
-      add(:base_fee_per_gas, :numeric, precision: 100, null: true)
-    end
-  end
-end
+#    alter table(:blocks) do
+#      add(:base_fee_per_gas, :numeric, precision: 100, null: true)
+#    end
+#  end
+#end
