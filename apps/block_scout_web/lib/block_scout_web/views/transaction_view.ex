1c1982a7cd29d27df4c51271aad14ac33b9fa884
Jaycelv <supercamel001@gmail.com>
2023-05-03 18:54:41 +0800

    fix: da gas price history data

--- apps/block_scout_web/lib/block_scout_web/views/transaction_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/transaction_view.ex
@@ -385,11 +385,7 @@ require Logger
   end
 
   def da_gas_price(%Transaction{da_gas_price: da_gas_price}, unit) when unit in ~w(wei gwei ether)a do
-    format_wei_value(da_gas_price, unit)
-  end
-
-  def da_gas_price(%Transaction{da_gas_price: nil}, unit) when unit in ~w(wei gwei ether)a do
-    format_wei_value(%Wei{value: Decimal.new(0)}, unit)
+    if da_gas_price == nil, do: format_wei_value(%Wei{value: Decimal.new(0)}, unit), else: format_wei_value(da_gas_price, unit)
   end
 
   def l2_fee(%Transaction{gas_price: gas_price, gas: gas}, unit) when unit in ~w(wei gwei ether)a do
