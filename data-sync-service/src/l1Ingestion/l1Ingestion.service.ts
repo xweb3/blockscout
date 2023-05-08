ab2f86c19b0d11bee8e1ae0acf72ab7101fa73d9
Jaycelv <supercamel001@gmail.com>
2023-05-08 14:31:51 +0800

    fix: hide debug code

--- data-sync-service/src/l1Ingestion/l1Ingestion.service.ts
+++ data-sync-service/src/l1Ingestion/l1Ingestion.service.ts
@@ -468,6 +468,7 @@ export class L1IngestionService {
       )
       console.log('res for debug hash', res)
       for (let i = 0; i < unMergeTxList.length; i++) {
+        console.log("current msg hash:", unMergeTxList[i].msg_hash)
         const l1ToL2Transaction = await this.getL1ToL2TxByMsgHash(
           unMergeTxList[i].msg_hash,
         )
