14d1d196c49c2a1132f86cb2083d129e75ecb3ec
dennisloh95 <dennisloh95@gmail.com>
2023-07-30 00:30:29 +0800

    chore: enhance tooltips behaviour

--- apps/block_scout_web/assets/js/lib/tooltip.js
+++ apps/block_scout_web/assets/js/lib/tooltip.js
@@ -1,5 +1,35 @@
 import $ from 'jquery'
 
+export const hideUnvisibleTooltips = () => {
+  if ($('.tooltip').length > 1) {
+    $('.tooltip').each((_, el) => {
+      const id = $(el).attr('id')
+      if (!$(`[aria-describedby=${id}]`).length) {
+        // console.log('will destroy')
+        $(el).hide()
+      }
+    })
+  }
+}
+
 $(function () {
   $('body').tooltip({ selector: '[data-toggle="tooltip"]' })
+
+  $('body').on('show.bs.tooltip', function (e) {
+    hideUnvisibleTooltips()
+  })
+
+  $('body').on('shown.bs.tooltip', function () {
+    // console.log('inserted.bs.tooltip')
+  })
+  $('body').on('hide.bs.tooltip', function () {
+    // console.log('hide.bs.tooltip')
+  })
+  $('body').on('hidden.bs.tooltip', function () {
+    // console.log('hidden.bs.tooltip')
+  })
+  $('body').on('inserted.bs.tooltip', function () {
+    hideUnvisibleTooltips()
+  })
+
 })
